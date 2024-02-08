db5de27992a5f9d8848adde184895beae6037f54
ArielCorrado <jarielc2012@yahoo.com.ar>
2024-02-08 16:53:24 -0300

    cm

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,22 @@
+import "./home.css";
+
+function Home() {
+    return (
+        <div className="homeMainCont flex">
+            <div className="homeMainTextCont flex column">
+                <div className="flex">
+                    <img src="/images/logo.png" alt="Logo" />
+                    <div className="homeMainDivider"></div>
+                    <div className="homeMainTexts flex column"> 
+                        <p className="homeMainText1">Parada</p>
+                        <p className="homeMainText1 cursive">técnica</p>
+                    </div>
+                </div>
+                <p className="homeMainText2">Actividades indoor y outdoors</p>
+            </div>
+            <img src="/images/homeMain.jpg" alt="Home" className="homeMainBgImg"/>    
+        </div>
+    )
+}
+
+export default Home
